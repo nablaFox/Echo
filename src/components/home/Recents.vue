7fa1ed002fbade04db6dfb25af931c1828bd0f50
nablaFox <a.finex04@gmail.com>
2023-02-03 19:33:18 +0100

    feat: splide implementation for 'recents' component

--- src/components/home/Recents.vue
+++ src/components/home/Recents.vue
@@ -1,29 +1,42 @@
+<script setup lang="ts">
+
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/vue-splide/css/core'
+
+</script>
+
+
 <template>
 
-    <div class="recents"> 
-        <div 
-            class="recent"
-            v-for="n in 3"
+    <Splide 
+        :options="{ 
+            arrows: false, 
+            fixedWidth: 'auto',
+            gap: '10px',
+            width: '100%',
+            pagination: false,
+        }" 
+        aria-label="My Favorite Images"
+    >
+        <SplideSlide
+            v-for="n in 10"
         >
-            <span class="name"> Room </span>
-            <span class="time"> #34h </span>
-        </div>
-    </div>
+            <div class="recent">
+                <span class="name"> Room </span>
+                <span class="time"> #34h </span>
+            </div>
+        </SplideSlide>
+    </Splide>
 
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
-
-.recents {
-	border-top: 1px solid var(--md-sys-color-outline-variant);
+.splide {
+    border-top: 1px solid var(--md-sys-color-outline-variant);
 	border-bottom: 1px solid var(--md-sys-color-outline-variant);
-	padding: 18px 20px;
-	@include flex($justify: start);
-	width: 100%;
-	gap: 20px;
-	@include maxHSize(719px) { padding: 13px 20px; }
-    overflow: hidden;
+    padding: 18px 20px;
+    @include maxHSize(719px) { padding: 13px 20px; }
 }
 
 .recent {
@@ -38,6 +51,5 @@
 }
 
 .time { color: var(--md-sys-color-primary) }
-
 
 </style>
