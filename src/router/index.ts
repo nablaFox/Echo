3c6f84e912365af7b53490bbe0573913fbc3d397
nablaFox <a.finex04@gmail.com>
2023-02-12 22:14:12 +0100

    fix: avoid infinite redirection when user it's not registered

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,42 +16,45 @@ router.beforeEach(async (to, from, next) => {
 		!await isLogin() 
 		&& to.name !== 'SignUp'
 		&& to.name !== 'SignIn'
-	) {
-		next({ name: 'SignUp' })
-	}
+	) { return next({ name: 'SignUp' }) }
+	// check if is logged
 
-	if (
-		to.name === 'SignUp'
-		|| to.name === 'SignIn'
-	) {
-		if (await isLogin()) return next({ name: 'Home' }) // redirect the user if is logged
-		return next() 
-	}
-	// if the user wants to login, there is no need to load data
-	
-	try { !userStore.isLoaded && await userStore.load() }
-	catch { return next({ name: 'SignUp' }) }
-	// if there is an error, since the user is logged
-	// it means that the user has yet to register
+	try {
+		await userStore.load()
 
-	if (
-		userStore.inTheRoom
-		&& to.name !== 'Room'
-	) {
-		return next({ 
+		if (
+			userStore.inTheRoom
+			&& to.name !== 'Room'
+		) next({ 
 			name: 'Room', 
 			params: { id: userStore.currentRoom.id }
 		})
-	} // if the user is in the room, always redirect him in the room
+		// if the user is in the room, always redirect him in the room
 
-	if (
-		userStore.isWaiting
-		&& to.name !== 'Lobby'
-	) {
-		return next({ name: 'Lobby' })
-	} // if the user is waiting, always redirect him in the lobby
+		else if (
+			userStore.isWaiting
+			&& to.name !== 'Lobby'
+		) next({ name: 'Lobby' }) 
+		// if the user is waiting, always redirect him in the lobby 
 
-	next()
+		else if (
+			to.name === 'SignUp'
+			|| to.name === 'SignIn'
+		) next({ name: 'Home' })
+		// since is logged, redirect the user
+
+		else next()
+	}
+	catch {
+		if (
+			to.name !== 'SignUp'
+			&& to.name !== 'SignIn'
+		) return next({ name: 'SignUp' })
+		
+		next()
+	}
+	// if there is an error, since the user is logged
+	// it means that the user has yet to register	
 })
 
 export default router
